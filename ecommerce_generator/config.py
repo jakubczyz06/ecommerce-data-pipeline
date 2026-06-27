e924dd20089b406e42a0ebafc52c795ad69172e5
jakub <czyzkuba124@gmail.com>
2026-06-27 18:37:58 +0200

    The final version of data generator

--- ecommerce_generator/config.py
+++ ecommerce_generator/config.py
@@ -29,9 +29,9 @@ CONFIG = {
         "num_orders":   8_000,
     },
     "LARGE": {
-        "label":        "Large (5,000 clients / 279 products / 50,000 orders)",
+        "label":        "Large (5,000 clients / 260 products / 50,000 orders)",
         "num_clients":  5_000,
-        "num_products":   279,
+        "num_products":   260,
         "num_orders":  50_000,
     },
 }
