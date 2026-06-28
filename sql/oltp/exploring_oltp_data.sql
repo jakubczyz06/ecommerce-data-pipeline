63fc9db56ce784ccfe11652dc91d5d87ef1bb892
jakub <czyzkuba124@gmail.com>
2026-06-29 00:23:31 +0200

    Created new queries

---
+++ sql/oltp/exploring_oltp_data.sql
@@ -0,0 +1,15 @@
+-- Making SELECT queries for each table
+SELECT *
+FROM oltp.client_addresses;
+
+SELECT *
+FROM oltp.clients;
+
+SELECT *
+FROM oltp.order_items;
+
+SELECT *
+FROM oltp.orders;
+
+SELECT *
+FROM oltp.products;
